9ba742abb6f4e1a5a4b8c59186d10efe777fc422
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2021-11-14 13:23:02 +0200

    Swaggler API

--- settings/bookstore_api/urls.py
+++ settings/bookstore_api/urls.py
@@ -3,17 +3,42 @@ from django.urls import include
 from rest_framework import routers
 from . import views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 router = routers.DefaultRouter()
 router.register(r'Publisher', views.PublisherViewSet, basename='Publisher')
 router.register(r'Author', views.AuthorViewSet, basename='Author')
 router.register(r'Book', views.BookViewSet, basename='Book')
 
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Bookstore API",
+      default_version='v1',
+      description="Api doc for MYFI Middle Python Developer Test. \
+                    Complited by Kopylov Evgene. t.me @EvgeneKopylov",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="eugenevkopylov@gmail.com"),
+      license=openapi.License(name="GNU_v3"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
-    path('', include(router.urls)),
+    path('items/', include(router.urls)),
     path('publishers_info/<int:page>/<int:size>/', views.publishers_info),
     path('authors_info/<int:page>/<int:size>/', views.authors_info),
     path('books_info/<int:page>/<int:size>/', views.books_info),
     path('api-auth/', include('rest_framework.urls',
-                              namespace='rest_framework'))
+                              namespace='rest_framework')),
+
+    path('', schema_view.with_ui('swagger',
+                                 cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+                                       cache_timeout=0), name='schema-redoc'),
+
 ]
--- settings/settings/settings.py
+++ settings/settings/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'bookstore_api',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
