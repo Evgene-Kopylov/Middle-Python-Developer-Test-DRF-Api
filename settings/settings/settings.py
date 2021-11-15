b58d6a7ee32529532566827cc2c80fc9bef66ae4
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2021-11-15 17:53:54 +0200

    Update settings.py

--- settings/settings/settings.py
+++ settings/settings/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
