12f9a9f4837bdea7595cf96215d4afe491c9c559
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2021-11-15 13:50:30 +0200

    test Publisher post

--- settings/bookstore_api/tests.py
+++ settings/bookstore_api/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+
+class BookstoreApiTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_post_publisher(self):
+        data = {
+            "name": "RT",
+            "description": "string11"
+        }
+        response = self.client.post("/items/edit_publisher/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
