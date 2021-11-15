83c82aa5bf94613b67e9c60f91ab64aca07d8965
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2021-11-15 14:42:34 +0200

    test GET POST items

---
+++ settings/bookstore_api/factory.py
@@ -0,0 +1,85 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from factory.faker import faker
+from faker.providers import DynamicProvider
+
+import datetime
+
+from bookstore_api.models import Publisher
+from bookstore_api.models import Author
+from bookstore_api.models import Book
+
+
+
+class MyFactory:
+
+    def fake_all(self):
+        self.fake = faker.Faker('Ru_RU')
+        fake = self.fake
+
+        book = Book(
+            title=fake.sentence(nb_words=3),
+            annotation=fake.sentence(nb_words=15),
+            isbn=fake.isbn13(),
+            publish_at=fake.date_between(
+                datetime.date.today() - datetime.timedelta(days=37000),
+                datetime.date.today()
+            ),
+            total_sells=fake.pyint(0, 1000, 1),
+            total_views=fake.pyint(0, 10000, 1),
+        )
+        book.save()
+
+        author = self.fake_author()
+
+        book.authors.add(author)
+
+        if book.id % 3 == 0:
+            author_2 = self.fake_author()
+            book.authors.add(author_2)
+        if book.id % 2 == 0:
+            _id = fake.pyint(1, len(Author.objects.all()), 1)
+            author_3 = Author.objects.filter(id=_id).first()
+            book.authors.add(author_3)
+
+        book.publisher = self.fake_publisher()
+        book.save()
+        return book
+
+
+    def fake_publisher(self):
+        publisher_provider = DynamicProvider(
+            provider_name="publisher",
+            elements=["РАН", "ЭКСМО", "Заря", "Донцова инкорпорейтед.", "РПЦ"],
+        )
+        fake = self.fake
+        fake.add_provider(publisher_provider)
+
+        name = fake.publisher()
+        publisher = Publisher.objects.filter(name=name).first()
+        if not publisher:
+            publisher = Publisher(
+                name=name,
+                description=fake.sentence(nb_words=10)
+            )
+            publisher.save()
+        return publisher
+
+    def fake_author(self):
+        fake = self.fake
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        second_name = fake.middle_name()
+
+        author = Author.objects.filter(
+            first_name=first_name,
+            last_name=last_name,
+            second_name=second_name).first()
+        if not author:
+            author = Author(
+                first_name=first_name,
+                last_name=last_name,
+                second_name=second_name)
+            author.save()
+        return author
+
--- settings/bookstore_api/tests.py
+++ settings/bookstore_api/tests.py
@@ -2,16 +2,67 @@ from rest_framework.test import APITestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 
+from .factory import MyFactory
+
 
 class BookstoreApiTestCase(APITestCase):
 
     def setUp(self):
         self.client = APIClient()
+        f = MyFactory()
+        f.fake_all()
+
+    def test_get_publisher(self):
+        response = self.client.get("/items/edit_publisher/1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_author(self):
+        response = self.client.get("/items/edit_author/1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_book(self):
+        response = self.client.get("/items/edit_book/1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
 
     def test_post_publisher(self):
         data = {
             "name": "RT",
             "description": "string11"
         }
+
         response = self.client.post("/items/edit_publisher/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_author(self):
+        data = {
+            "first_name": "Александр",
+            "last_name": "Пушкин",
+            "second_name": "Сергеевич"
+        }
+
+        response = self.client.post("/items/edit_author/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_book(self):
+        data = {
+            "title": "Книга ЩЩФЗ",
+            "annotation": "загадочная",
+            "isbn": "123456789123",
+            "publish_at": "2021-11-15",
+            "total_sells": 0,
+            "total_views": 0,
+            "authors": [
+                1
+            ],
+            "publisher": 1
+        }
+
+        response = self.client.post("/items/edit_book/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+
+
+
+
